c3b2855de032d190600bc89af80f36a60a79c1bb
Nat <lohweirunatalie@gmail.com>
2026-06-13 22:42:31 +0800

    debugging\

--- MyLittleShop/scenes/interactables/door.gd
+++ MyLittleShop/scenes/interactables/door.gd
@@ -4,7 +4,7 @@ class_name Door
 @export var target_scene: String
 @export var target_spawn_id: String
 
-func interact():
+func interact(player: Player):
 	SceneManager.change_scene(
 		target_scene,
 		target_spawn_id
