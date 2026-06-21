6575c15e43f12b3961886d1e0ba88cf22d821d41
vinhee <115960216+vinhee@users.noreply.github.com>
2026-06-21 22:58:23 +0800

    Updated stone collectable

--- MyLittleShop/scenes/objects/rocks/rock.gd
+++ MyLittleShop/scenes/objects/rocks/rock.gd
@@ -3,6 +3,7 @@ extends Sprite2D
 @onready var damageable_area: DamageableArea = $DamageableArea
 @onready var damage_component: DamageComponent = $DamageComponent
 
+var stone_scene = preload("res://scenes/objects/rocks/stone.tscn")
 # note for now rock uses same tool to break as trees
 
 func _ready() -> void:
@@ -17,6 +18,12 @@ func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
 
 func on_max_damage_reached() -> void:
+	call_deferred("add_stone_scene")
 	print("max dmg rched")
-	# remove the scene aka "tree dies"
+	# remove the scene aka "rock dies"
 	queue_free()
+
+func add_stone_scene() -> void:
+	var stone_instance = stone_scene.instantiate() as Node2D
+	stone_instance.global_position = global_position
+	get_parent().add_child(stone_instance)
