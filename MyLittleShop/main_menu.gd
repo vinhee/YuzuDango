4388748e19c0bde7f8f69243dae8c9e8253aa987
vinhee <115960216+vinhee@users.noreply.github.com>
2026-06-24 10:37:06 +0800

    Added Main Menu

---
+++ MyLittleShop/main_menu.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world/MainMap.tscn")
+
+#func _on_load_pressed() -> void:
+	#change scene to load files
+	#get_tree().change_scene_to_file()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
