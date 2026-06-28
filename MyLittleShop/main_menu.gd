8d10a05cfc16bcf1b688c46a3f1b04ee15a5dd81
vinhee <115960216+vinhee@users.noreply.github.com>
2026-06-29 00:33:29 +0800

    update conflicts

--- MyLittleShop/main_menu.gd
+++ MyLittleShop/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world/MainMap.tscn")
+	get_tree().change_scene_to_file("res://scenes/world/Game.tscn")
 
 #func _on_load_pressed() -> void:
 	#change scene to load files
