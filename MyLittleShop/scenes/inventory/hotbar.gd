c9eb1e321d48426198a3317919a2512b827648a9
vinhee <115960216+vinhee@users.noreply.github.com>
2026-06-29 00:29:18 +0800

    add in hotbar selection system

--- MyLittleShop/scenes/inventory/hotbar.gd
+++ MyLittleShop/scenes/inventory/hotbar.gd
@@ -3,12 +3,34 @@ extends HBoxContainer
 @onready var inventory: Inventory = preload("res://scenes/player/playerInventory.tres")
 @onready var slots: Array = get_children()
 
+var active_slot_index: int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	inventory.updated.connect(update)
 	update()
 
+func _unhandled_input(event: InputEvent) -> void:
+	var old_index = active_slot_index
+
+	if event.is_action_pressed("scroll_up") or event.is_action_pressed("hotbar_prev"):
+		active_slot_index = posmod(active_slot_index - 1, slots.size())
+	elif event.is_action_pressed("scroll_down") or event.is_action_pressed("hotbar_next"):
+		active_slot_index = posmod(active_slot_index + 1, slots.size())
+
+	for i in range(slots.size()):
+		if event.is_action_pressed("hotbar_" + str(i + 1)):
+			active_slot_index = i
+			
+	if old_index != active_slot_index:
+		update_selection_visuals()
+
 func update() -> void:
 	for i in range(slots.size()):
 		var inventory_slots: InventorySlot = inventory.slots[i]
 		slots[i].update_to_slot(inventory_slots)
+
+func update_selection_visuals() -> void:
+	for i in range(slots.size()):
+		if slots[i] is HotbarSlot:
+			slots[i].set_selected(i == active_slot_index)
--- MyLittleShop/scenes/inventory/hotbar_slot.gd
+++ MyLittleShop/scenes/inventory/hotbar_slot.gd
@@ -4,13 +4,28 @@ class_name HotbarSlot
 @onready var backgroundSprite: Sprite2D = $background
 @onready var item_stack_gui: ItemStack = $CenterContainer/Panel
 
+var current_slot_data: InventorySlot = null
+var is_selected: bool = false
+
 func update_to_slot(slot: InventorySlot) -> void:
-	if !slot || !slot.item:
+	current_slot_data = slot
+	_refresh_visuals()
+	
+func set_selected(selected: bool) -> void:
+	is_selected = selected
+	_refresh_visuals()
+
+func _refresh_visuals() -> void:
+	if !current_slot_data || !current_slot_data.item:
 		item_stack_gui.visible = false
-		backgroundSprite.frame = 0
-		return
-		
-	item_stack_gui.inventorySlot = slot
-	item_stack_gui.update()
-	item_stack_gui.visible = true
-	backgroundSprite.frame = 1
+	else:
+		item_stack_gui.inventorySlot = current_slot_data
+		item_stack_gui.update()
+		item_stack_gui.visible = true
+
+	if is_selected:
+		backgroundSprite.frame = 2
+	else:
+		# Normal frames
+		backgroundSprite.self_modulate = Color.WHITE
+		backgroundSprite.frame = 1 if (current_slot_data and current_slot_data.item) else 0
