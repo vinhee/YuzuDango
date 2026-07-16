f83df47028914b07ef065e6f04b0c78896eeefc9
Natalie <lohweirunatalie@gmail.com>
2026-07-16 17:29:29 +0800

    Added money.gd

---
+++ MyLittleShop/scripts/globals/money.gd
@@ -0,0 +1,16 @@
+extends Node
+
+signal money_changed(new_amount: int)
+
+var amount: int = 0
+
+func add(value: int) -> void:
+	amount += value
+	money_changed.emit(amount)
+
+func spend(value: int) -> bool:
+	if amount >= value:
+		amount -= value
+		money_changed.emit(amount)
+		return true
+	return false
